f20b256f03c2b7490f699baa35a1ff343afc071a
maxkrieger <themaxaxis@gmail.com>
2020-08-09 21:50:17 -0400

    Updates

---
+++ precache-manifest.beb18affb951f2f27dd033102d838b38.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2c6cbfcf25cafff87f544e7d41c39cf",
+    "url": "/saga-alpha/index.html"
+  },
+  {
+    "revision": "741966a10b89ebf8c67a",
+    "url": "/saga-alpha/static/css/2.c2bc211b.chunk.css"
+  },
+  {
+    "revision": "bffedd042acd8b93cf3f",
+    "url": "/saga-alpha/static/css/main.39872602.chunk.css"
+  },
+  {
+    "revision": "741966a10b89ebf8c67a",
+    "url": "/saga-alpha/static/js/2.c1decf35.chunk.js"
+  },
+  {
+    "revision": "9bb61d550eedd94f3f54be6698819736",
+    "url": "/saga-alpha/static/js/2.c1decf35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bffedd042acd8b93cf3f",
+    "url": "/saga-alpha/static/js/main.390772db.chunk.js"
+  },
+  {
+    "revision": "1629baad5a72347f7dd0",
+    "url": "/saga-alpha/static/js/runtime-main.4e23751b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f52ffc8391bef9cf76be04661ff0ae78.js"
+  "/saga-alpha/precache-manifest.beb18affb951f2f27dd033102d838b38.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/saga-alpha/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
